1bd230c81c7027c83143ccd9c4ba1715acaefc02
Lucas Caetano <lucascostacaetanofla@gmail.com>
2022-03-04 16:37:05 -0300

    Desafio MovieFlix capitulo6

---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,53 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
+	
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new MovieDTO(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<MovieCardDTO> findByGenre(Long genreId, Pageable pageable) {
+		
+		List<Genre> genres = (genreId == 0) ? null : Arrays.asList(genreRepository.getOne(genreId));
+		Page<Movie> page = repository.find(genres, pageable);
+		return page.map(x -> new MovieCardDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findReviewFromMoview(Long id) {
+		Movie movie = repository.getOne(id);
+		return movie.getReviews().stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+	}
+}
